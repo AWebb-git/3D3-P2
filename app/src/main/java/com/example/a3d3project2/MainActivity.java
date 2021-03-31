07c71935f22f1ddf63e8bc230a506ac2a7f586a7
AWebb-git <79225323+AWebb-git@users.noreply.github.com>
2021-03-31 20:54:15 +0100

    directory response saving, updating and app exiting

--- app/src/main/java/com/example/a3d3project2/MainActivity.java
+++ app/src/main/java/com/example/a3d3project2/MainActivity.java
@@ -2,21 +2,16 @@ package com.example.a3d3project2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.net.wifi.WifiInfo;
-import android.net.wifi.WifiManager;
-import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Bundle;
-import android.text.format.Formatter;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.lang.reflect.Array;
 import java.net.Inet4Address;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
@@ -24,20 +19,24 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketException;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Enumeration;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-public static final String Dest_IP = "Your IP";
-public static final int Dest_Port = 1201;  //ENTER whatever your dest port is
+public static final String Dest_IP = "YOUR dest IP";
+public static final int Dest_Port = 12000;  //ENTER whatever your dest port is
 public Socket socket;
 public TextView response;
 public TextView info;
 public String message;
 public String Source_IP = "null";
 public int Source_Port = 1201;  //random port number
-public String dir_IP = "YOUR Directory IP";
+public String dir_IP = "YOUR DIR IP";
 public int dir_port = 12000;
-public String list = null;
+public ArrayList<String> nodes;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,20 +47,33 @@ public String list = null;
         info = (TextView)findViewById(R.id.textView3);
         Source_IP = getLocalIpAddress();
         info.setText(Source_IP);
+        nodes = new ArrayList<String>();
+
         new Thread(new RecThread()).start();
         dirConnect();
     }
 
     public void dirConnect(){
         String portMsg = String.valueOf(Source_Port);
-        new Thread(new SendThread(portMsg, dir_IP, dir_port)).start();
+        new Thread(new SendThread(portMsg, dir_IP, dir_port, 1)).start();
+    }
+
+    public void dirUpdate(View view){
+        String updateMsg = "Update";
+        new Thread(new SendThread(updateMsg, dir_IP, dir_port, 1)).start();
+    }
+
+    public void dirExit(View view){
+        String portMsg = String.valueOf(Source_Port);
+        new Thread(new SendThread(portMsg, dir_IP, dir_port, 0)).start();
+        finishAndRemoveTask();
     }
 
     //runs on button click
     public void sendMsgBtn(View view){
         EditText msg = (EditText)findViewById(R.id.EditText);
         message = msg.getText().toString().trim();
-        new Thread(new SendThread(message, Dest_IP, Dest_Port)).start();
+        new Thread(new SendThread(message, Dest_IP, Dest_Port, 0)).start();
     }
 
     class RecThread implements Runnable{
@@ -95,16 +107,18 @@ public String list = null;
         private String message;
         private String dest_ip;
         private int dest_port;
-        SendThread(String message, String dest_ip, int dest_port){
+        private int access_arr; //if one update nodes arraylist
+        SendThread(String message, String dest_ip, int dest_port, int access_arr){
             this.message = message;
             this.dest_ip = dest_ip;
             this.dest_port = dest_port;
+            this.access_arr = access_arr;
         }
         @Override
         public void run() {
             InetAddress destIP = null;
-            PrintWriter outputSend = null;
-            BufferedReader inputSend = null;
+            PrintWriter outputSend;
+            BufferedReader inputSend;
             try {
                 destIP = InetAddress.getByName(dest_ip);
             } catch (UnknownHostException e) {
@@ -117,12 +131,19 @@ public String list = null;
                 outputSend.write(message);
                 outputSend.flush();
                 socket.shutdownOutput();
-                list = inputSend.readLine();
+                if(access_arr == 1){    //if requesting connection or update from directory, update node list
+                    String temp = inputSend.readLine();
+                    nodes.clear();
+                    nodes = new ArrayList<String>(Arrays.asList(temp.split(" ")));
+                }
             } catch (IOException e) {
                 response.setText("Error IO");
             }
+            int i = 0;
         }
     }
+
+
 
 
     //gotten from https://stackoverflow.com/questions/6064510/how-to-get-ip-address-of-the-device-from-code
